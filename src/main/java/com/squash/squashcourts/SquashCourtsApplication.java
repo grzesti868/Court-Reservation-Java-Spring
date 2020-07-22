ab0a1ed2c4ce3fda2ec3f6d979cfe257538bd8f4
grzesti868 <gregustich@gmail.com>
2020-07-22 22:22:53 +0200

    Squash courts first commit

---
+++ src/main/java/com/squash/squashcourts/SquashCourtsApplication.java
@@ -0,0 +1,13 @@
+package com.squash.squashcourts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquashCourtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquashCourtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squash/squashcourts/SquashCourtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squash.squashcourts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquashCourtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
